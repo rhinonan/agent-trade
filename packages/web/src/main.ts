4bd08a4531e9a4d6995b881f9ffdefaad656964e
rhinonan <rhinonan@163.com>
2026-06-21 10:35:29 +0800

    chore: add primevue dependency and configure unstyled mode

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
 import App from "./App.vue";
 import "tailwindcss";
 
 const app = createApp(App);
 app.use(createPinia());
+app.use(PrimeVue, {
+  unstyled: true,
+});
 app.mount("#app");
