76ecd1f80f702756526bbf386c4b4e165767cebe
rhinonan <rhinonan@163.com>
2026-06-21 08:24:42 +0800

    feat(web): wrap FlowView sections in glass panels with glow

--- packages/web/src/components/FlowView.vue
+++ packages/web/src/components/FlowView.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="p-5 flex flex-col gap-6 flex-1">
-    <StepProgress :steps="store.steps" />
-    <LiveLog :logs="store.logs" :is-running="store.isRunning" />
+    <div class="glass-panel-glow p-5">
+      <StepProgress :steps="store.steps" />
+    </div>
+    <div class="glass-panel p-5">
+      <LiveLog :logs="store.logs" :is-running="store.isRunning" />
+    </div>
   </div>
 </template>
 
